3a8423efc4c7aeae45c8466a59bc27e572174a8f
casualcas <nininbini111@gmail.com>
2026-04-22 13:02:03 +0000

    Update Supabase to new project for 2048 PvP

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://khopjdqiwetyyydyosij.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoaG9wamRxaXdldHl5ZHlvc2lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzODA0MjgsImV4cCI6MjA4OTk1NjQyOH0.9pHFlQVlFytrW_99J42UCXUVdRh1EJ6xZsFrMFCE6yg';
-
+const SUPABASE_URL = 'https://dszmjuinurczqxatfufd.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzem1qdWludXJjenF4YXRmdWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NTQxNTIsImV4cCI6MjA5MjQzMDE1Mn0.k2k6C3xCxJ1ZL2qrJ4V-Oq7rKlNdqSHyMIJQFum03ZE';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   realtime: {
     params: {
